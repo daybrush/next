fa1383599309e2d19032ed5550cfc50626448f79
itoolsg <itoolsg@gmail.com>
2013-03-21 23:52:09 +0900

    finish 3-1
    finish

--- pp3-1/exercise6.py
+++ pp3-1/exercise6.py
@@ -4,15 +4,14 @@ str=raw_input("input String : ")
 
 length=len(str)
 
-half_length=length/2
+half_length=length/2#half length
 
 compare=True
 
 for i in range(0,half_length):
-    if(str[i]!=str[length-1-i]):
+    if(str[i]!=str[length-1-i]): # 0 ~ half  half ~ length compare
         compare=False;
         break;
-
 
 if(compare):
     print("same");
